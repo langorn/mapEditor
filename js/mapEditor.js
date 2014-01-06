c34765fe8e6f2e13c6e7ae4b15483edcc4ffa91b
Mark <raynor.tags@gmail.com>
2014-01-06 15:10:22 +0800

    make the processing of create a shape by line , and then remove it , recreate by the shape

--- js/mapEditor.js
+++ js/mapEditor.js
@@ -8,6 +8,7 @@ var mapEditor = {
    layer:null,
    group:null,
    lineGroup:null,
+   lastLinePoint:null,
    init:function(){
          // if(stage!=null){
          //    stage.clear();
@@ -20,8 +21,9 @@ var mapEditor = {
           })
           console.log(stage);
           this.loadBackground(stage);
-
-
+          //this.zoneName();
+          //this.endZone();
+          
    },
    loadBackground:function(stage){
          // Create a new layer
@@ -42,6 +44,7 @@ var mapEditor = {
               y: 10,
               image: imageobj,
             });
+            
             group.add(img)
             layer.add(group);
             stage.add(layer);
@@ -63,14 +66,13 @@ var mapEditor = {
           mapEditor.plot.push(mousePos.x);
           mapEditor.plot.push(mousePos.y);
           var plotCount=mapEditor.plot.length;
-
           mapEditor.drawTheLine(mapEditor.plot);
            
         }, false);
     
    },
    drawTheLine:function(plot){
-
+      var that = this;
       var greenLine = new Kinetic.Line({
         x: 0,
         y: 0,
@@ -81,12 +83,94 @@ var mapEditor = {
         strokeWidth: 3,
         lineCap: 'round',
         lineJoin: 'round',
-        dashArray: [1,2]
+        dashArray: [1,2],
       }); 
 
-      lineGroup.add(greenLine)
+      lastLinePoint = greenLine.getPoints();
+      lineGroup.add(greenLine);
       layer.add(lineGroup);
+      
+      //var x = greenLine.intersects(greenLine);
+      //console.log(x);
       stage.add(layer);
+      that.drawDone();
+   },
+   redrawShape:function(things){
+        console.log(things[0]);
+
+      var triangle = new Kinetic.Shape({
+      drawFunc: function(context) {
+          context.beginPath();
+          context.moveTo(things[0].x, things[0].y);
+          for(var i=1;i<things.length;i++){
+            context.lineTo(things[i].x,things[i].y)
+          }
+          context.closePath();
+          // KineticJS specific context method
+          context.fillStrokeShape(this);
+        },
+        fill: '#00D2FF',
+        stroke: 'black',
+        strokeWidth: 4,
+        lineJoin:'round',
+        opacity: 0.1
+      });
+      mapEditor.zoneName('Zone A');
+      layer.add(triangle);
+      
+      stage.add(layer);
+
+
+   },
+   zoneName:function(tooltipDesc){
+      // tooltip
+      var lastX = lastLinePoint[0].x
+      var lastY = lastLinePoint[0].y
+      var tooltip = new Kinetic.Label({
+        x: lastX,
+        y: lastY,
+        opacity: 0.75
+      });
+
+      tooltip.add(new Kinetic.Tag({
+        fill: 'black',
+        pointerDirection: 'down',
+        pointerWidth: 10,
+        pointerHeight: 10,
+        lineJoin: 'round',
+        shadowColor: 'black',
+        shadowBlur: 10,
+        shadowOffset: 10,
+        shadowOpacity: 0.5
+      }));
+      
+      tooltip.add(new Kinetic.Text({
+        text: tooltipDesc,
+        fontFamily: 'Calibri',
+        fontSize: 18,
+        padding: 5,
+        fill: 'white'
+      }));
+      group.add(tooltip)
+      layer.add(group);
+      stage.add(layer);
+
+   },
+   drawDone:function(){
+
+      var drawDone = document.getElementById('drawDone');
+      drawDone.addEventListener('click',function(){
+          //alert('green line remove');
+          //lineGroup.remove();
+          
+          layer.remove(lineGroup);
+          //layer.draw();
+          mapEditor.redrawShape(lastLinePoint);
+      })
+   },
+   quickLoad:function(){
+
+
    }
 }
 
@@ -98,6 +182,9 @@ var mapEditor = {
       y: evt.clientY - rect.top
     };
   }
+
+
+
 
 
  mapEditor.init();
